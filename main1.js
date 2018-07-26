455a40cf14bf4d2b2f9cffc5473c3475090a4834
NatkaDzoll <natalia.zholud@mail.ru>
2018-07-26 23:05:11 +0300

    Reload img and some change

--- main1.js
+++ main1.js
@@ -1,40 +1,48 @@
     "use strict";
 
 var imgs = document.getElementsByTagName('img');
+var zIndex = 1;
 
 for (var i = 0; i<imgs.length; i++){
 	imgs[i].addEventListener('mousedown', moveByMousedown, false);
-	console.log( imgs);
 }
-var zIndex = 1;
 
-	function moveByMousedown(EO) {		// клик на переносимый объект, начать перенос
-		EO = EO || window.event;
+
+function moveByMousedown(EO) {	// клик на переносимый объект, начать перенос
+	EO = EO || window.event;
+	
+	if (EO.which == '1') {
 		var img = EO.target;
 		var posImg = getElemPosition(img)
 		var imgClickX = Math.round(EO.pageX - posImg.left);
 		var imgClickY = Math.round(EO.pageY - posImg.top);
 		img.style.position = 'absolute';
 		img.style.zIndex = zIndex;
-		moveAt(EO);
-
-		document.onmousemove = function (EO) {		 //отобразить перенос объекта
-			moveAt(EO);
-		}
-        
-		function moveAt(EO) {		// изменение координат переносимого объекта
-			img.style.left = EO.pageX - imgClickX +'px';
-			img.style.top = EO.pageY - imgClickY + 'px';
-		}
-		
-		img.onmouseup = function () {		 // отмена переноса по mouseup
-			document.onmousemove = null;
-			document.onmouseup = null;
-			//document.onmousedown = null;
-			zIndex++;
-		 }
-		  return false;
 	}
+
+	document.onmousemove = function (EO) {	 //отобразить перенос объекта
+		moveAt(EO);
+		changeCursor();
+		return false;
+	}
+
+	function changeCursor() {  // стилизация курсора при mousedown
+		document.body.style.cursor = 'move';
+	}
+
+	function moveAt(EO) {		// изменение координат переносимого объекта
+		//window.event.stopPropagation();
+		img.style.left = EO.pageX - imgClickX +'px';
+		img.style.top = EO.pageY - imgClickY + 'px';
+	}
+	
+	img.onmouseup = function () {		 // отмена переноса по mouseup
+		document.onmousemove = null;
+		document.onmouseup = null;
+		document.body.style.cursor = 'default';
+		zIndex++;
+	 }
+}
 
 //найдем положение клика относительно картинки
 function getElemPosition(elem) {
